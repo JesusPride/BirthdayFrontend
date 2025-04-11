2d6fc04195c4b8a4e49859b3d671c41e2fcde5db
JESUSPRIDE <jesuspride005@gmail.com>
2025-04-11 16:59:11 +0100

    fixed: navbar in birthday-wishes

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,7 @@ export default {
       return this.$route.path.startsWith('/dashboard')
     },
     isBirthdayCountdown() {
-      return this.$route.path === '/birthday'
+      return this.$route.path.startsWith('/birthday')
     }
   },
   setup() {
