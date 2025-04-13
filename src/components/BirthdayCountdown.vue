6a30563167828955e13d8e4731636dfcfc57a958
JESUSPRIDE <jesuspride005@gmail.com>
2025-04-13 18:59:35 +0100

    Fixed birthday date

--- src/components/BirthdayCountdown.vue
+++ src/components/BirthdayCountdown.vue
@@ -78,7 +78,7 @@ export default {
     return { themeStore };
   },
   data() {
-    const birthday = new Date("2025-04-09T15:54:00");
+    const birthday = new Date("2025-04-14T00:00:00");
     console.log("Birthday date initialized:", birthday.toISOString());
 
     // Load photos from localStorage
